9d314c7d7126257353040a50b54aaa0bd5e6ef1b
DaedalusHatak <74432776+DaedalusHatak@users.noreply.github.com>
2021-06-10 12:17:29 +0200

    Add files via upload

--- skrrty/main.cpp
+++ skrrty/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
-#include <algorithm>
+#include <algorithm>  //
 #include <string>
 #include <conio.h>
+#include <cstdlib>
+#include <ctime>    //srand
+#include <windows.h>  //sleep
+#include <thread>
+#include <limits>
 using namespace std;
     bool zycie=true;
     int x=1,y=1,c=0, d=0;
+    int potion=10,p=0;
+    int post=0;
+    int hp = 30, str=10;
+    int rat=10,ratstr=9;
     string komenda;
     char kom;
     void kw1();
@@ -31,48 +40,58 @@ using namespace std;
     void mapa10();
     void mapa11();
     void mapa12();
+    void walka();
+    void przedmiot();
+    void show();
     void gra();
     void gra1();
 int main()
 {
+    mapa1();
     cout<<"Witam w prostej grze RPG uzywaj klawiszy N,S,E,W do poruszania sie"<<endl;
     cout<<"N = NORTH"<<endl;
     cout<<"E = EAST"<<endl;
     cout<<"W = WEST"<<endl;
     cout<<"S = SOUTH"<<endl;
+    cout<<"U = UZYJ"<<endl;
+    cout<<"F = PODNIES"<<endl;
+    cout<<"SHOW = POKAZ PRZEDMIOTY"<<endl;
     cout<<"I = MAPA"<<endl<<endl;
     cout<<"Mozesz udac sie w kierunku NORTH lub EAST, Gdzie chcesz sie udac?"<<endl<<endl;
     gra();
-
 }
 void gra()
 {
-  void gra1();
+    ;
     while(zycie!=false)
     {
-        cout<<"Komenda: ";
-        cin>>komenda;
+
+                cout<<"Komenda: ";
+                cin>>komenda;
+
+
+
         cout<<endl;
         transform(komenda.begin(),komenda.end(),komenda.begin(),::tolower);
-        if(komenda=="n")
+        if(komenda=="n" || komenda=="north")
         {
             x++;
             kom='n';
             gra1();
         }
-        else if(komenda=="s")
+        else if(komenda=="s" || komenda=="south")
         {
             x--;
             kom='s';
             gra1();
         }
-        else if(komenda=="e")
+        else if(komenda=="e" || komenda=="east")
         {
             y++;
             kom='e';
             gra1();
         }
-        else if(komenda=="w")
+        else if(komenda=="w" || komenda=="west")
         {
             y--;
             kom='w';
@@ -83,7 +102,32 @@ void gra()
             kom='i';
             gra1();
         }
+        else if(komenda=="hp")
+        {
+            kom='h';
+
+            gra1();
+        }
+        else if(komenda=="f")
+        {
+            kom='f';
+
+            gra1();
+        }
+        else if(komenda=="u")
+        {
+            kom='u';
+
+            gra1();
+        }
+        else if(komenda=="show")
+        {
+            kom='p';
+
+            gra1();
+        }
     }
+
 }
 void gra1()
 {
@@ -152,6 +196,7 @@ void gra1()
     }
         cout<<"Brak drogi"<<endl<<endl; y--; break;
     }
+
 }
 
 
@@ -176,6 +221,12 @@ void kw1()
         cout<<"Udales sie w kierunku WEST"<<endl<<endl;
         cout<<"Mozesz udac sie w kierunku NORTH lub EAST, Gdzie chcesz sie udac?"<<endl<<endl;
     }
+    else if(kom=='h')
+    {
+       cout<<"Twoje zycie wynosi: "<<hp<<endl<<endl;
+    }
+    else if(kom=='u') przedmiot();
+    else if(kom=='p') show();
 
 }
 void kw2()
@@ -199,6 +250,12 @@ void kw2()
         cout<<"kw2"<<endl<<endl;
         cout<<"Mozesz udac sie w kierunku NORTH lub SOUTH, Gdzie chcesz sie udac?"<<endl<<endl;
     }
+    else if(kom=='h')
+    {
+       cout<<"Twoje zycie wynosi: "<<hp<<endl<<endl;
+    }
+    else if(kom=='u') przedmiot();
+    else if(kom=='p') show();
 
 }
 void kw3()
@@ -223,11 +280,27 @@ void kw3()
         cout<<"Mozesz udac sie w kierunku SOUTH lub EAST, Gdzie chcesz sie udac?"<<endl<<endl;
 
     }
+    else if(kom=='h')
+    {
+       cout<<"Twoje zycie wynosi: "<<hp<<endl<<endl;
+    }
+
+
+    else if(kom=='f')
+    {
+       p=1;
+       cout<<"Podniosles napoj uzdrawiajacy"<<endl<<endl;
+    }
+    else if(kom=='u') przedmiot();
+    else if(kom=='p') show();
+    if(p==0)
+    cout<<"Widzisz na ziemi przedmiot uzyj 'f' zeby podniesc"<<endl<<endl;
 }
 void kw4()
 {
     system("cls");
     mapa4();
+
         if(kom=='e')
     {
         cout<<"Udales sie w kierunku EAST"<<endl<<endl;
@@ -240,6 +313,13 @@ void kw4()
         cout<<"kw4"<<endl<<endl;
         cout<<"Mozesz udac sie w kierunku WEST, Gdzie chcesz sie udac?"<<endl<<endl;
     }
+    else if(kom=='h')
+    {
+       cout<<"Twoje zycie wynosi: "<<hp<<endl<<endl;
+    }
+    else if(kom=='u') przedmiot();
+    else if(kom=='p') show();
+    if(rat>0)walka();
 }
 void kw5()
 {
@@ -277,6 +357,12 @@ void kw6()
         cout<<"kw6"<<endl<<endl;
         cout<<"Mozesz udac sie w kierunku NORTH lub EAST lub WEST, Gdzie chcesz sie udac?"<<endl<<endl;
     }
+    else if(kom=='h')
+    {
+       cout<<"Twoje zycie wynosi: "<<hp<<endl<<endl;
+    }
+    else if(kom=='u') przedmiot();
+    else if(kom=='p') show();
 }
 void kw7()
 {
@@ -303,6 +389,12 @@ void kw8()
         cout<<"kw8"<<endl<<endl;
         cout<<"Mozesz udac sie w kierunku NORTH, Gdzie chcesz sie udac?"<<endl<<endl;
     }
+    else if(kom=='h')
+    {
+       cout<<"Twoje zycie wynosi: "<<hp<<endl<<endl;
+    }
+    else if(kom=='u') przedmiot();
+    else if(kom=='p') show();
 }
 void kw9()
 {
@@ -325,6 +417,12 @@ void kw9()
         cout<<"kw9"<<endl<<endl;
         cout<<"Mozesz udac sie w kierunku SOUTH lub WEST, Gdzie chcesz sie udac?"<<endl<<endl;
     }
+    else if(kom=='h')
+    {
+       cout<<"Twoje zycie wynosi: "<<hp<<endl<<endl;
+    }
+    else if(kom=='u') przedmiot();
+    else if(kom=='p') show();
 }
 void kw10()
 {
@@ -347,6 +445,12 @@ void kw10()
         cout<<"kw10"<<endl<<endl;
         cout<<"Mozesz udac sie w kierunku SOUTH lub NORTH, Gdzie chcesz sie udac?"<<endl<<endl;
     }
+    else if(kom=='h')
+    {
+       cout<<"Twoje zycie wynosi: "<<hp<<endl<<endl;
+    }
+    else if(kom=='u') przedmiot();
+    else if(kom=='p') show();
 }
 void kw11()
 {
@@ -369,6 +473,12 @@ void kw11()
         cout<<"kw11"<<endl<<endl;
         cout<<"Mozesz udac sie w kierunku SOUTH lub EAST, Gdzie chcesz sie udac?"<<endl<<endl;
     }
+    else if(kom=='h')
+    {
+       cout<<"Twoje zycie wynosi: "<<hp<<endl<<endl;
+    }
+    else if(kom=='u') przedmiot();
+    else if(kom=='p') show();
 }
 void kw12()
 {
@@ -539,4 +649,58 @@ void mapa12()
             cout<<"|     |     |     |"<<endl;
             cout<<"|     |     |     |"<<endl;
             cout<<"|_____|_____|_____|"<<endl;
+}
+void walka()
+{
+
+    srand(time(NULL));
+    while(rat>0)
+    {
+        ratstr=rand()%9;
+        str=rand()%12;
+        hp=hp-ratstr;
+        rat=rat-str;
+        cout<<"Zadajesz "<<str<<" obrazen"<<endl;
+        cout<<"Otrzymujesz "<<ratstr<<" obrazen"<<endl;
+        if(rat<=0)
+        {
+            cout<<"Szczur nie zyje"<<endl<<endl;
+        }
+        else if(hp<=0)
+            {cout<<"Nie zyjesz. Koniec gry"<<endl<<endl;zycie=false;break;}
+    }
+
+}
+void przedmiot()
+{
+    if(kom=='u' && p==1)
+    {
+        if (hp==30)
+       {
+           cout<<"Masz pelne zycie"<<endl<<endl;
+       }
+       else if (hp>=20)
+       {
+           hp=30;
+           p=2;
+           cout<<"Masz "<<hp<<" zycia"<<endl<<endl;
+       }
+       else if(hp<20)
+       {
+           hp=hp+potion;
+           p=2;
+           cout<<"Masz "<<hp<<" zycia"<<endl<<endl;
+       }
+
+    }
+    else if(kom=='u' && p==2)
+    cout<<"Nic nie masz"<<endl<<endl;
+}
+void show()
+{
+    if(p==1)
+    {
+        cout<<"Posiadasz 1 napoj uzdrawiajacy"<<endl<<endl;
+    }
+    else cout<<"Nie masz zadnych przedmiotow"<<endl<<endl;
 }
